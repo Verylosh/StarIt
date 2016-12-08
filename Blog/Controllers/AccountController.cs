23e76f382a5ab228f6d4f55f65f5f3f4440c301d
Martin Shishkov <verylosh@gmail.com>
2016-12-08 13:28:08 +0200

    1/2 allow migrations
    1/2 allow migrations

--- Blog/Controllers/AccountController.cs
+++ Blog/Controllers/AccountController.cs
@@ -151,8 +151,14 @@ namespace Blog.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser
+                {
+                    UserName = model.UserName,
+                    Email = model.Email
+                };
+
                 var result = await UserManager.CreateAsync(user, model.Password);
+
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
