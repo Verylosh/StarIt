fd54ee3ef98a453633482657f72933aae1ce9b0d
Martin Shishkov <verylosh@gmail.com>
2016-12-08 14:00:04 +0200

    fixed Users+add Place
    Fixed User names and
    Added empty places
    -controler
    -view
    -model

--- Blog/Startup.cs
+++ Blog/Startup.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.Owin;
+﻿using System.Data.Entity;
+using Blog.Models;
+using Microsoft.Owin;
+using Newtonsoft.Json.Converters;
 using Owin;
+using Blog.Migrations;
+using Blog.Models;
 
 [assembly: OwinStartupAttribute(typeof(Blog.Startup))]
 namespace Blog
@@ -8,6 +13,10 @@ namespace Blog
     {
         public void Configuration(IAppBuilder app)
         {
+
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+
             ConfigureAuth(app);
         }
     }
