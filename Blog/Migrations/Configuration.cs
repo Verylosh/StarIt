24e2cbcb0ea8afbf6ec54bed2aff2c7906567cc6
Martin Shishkov <verylosh@gmail.com>
2016-12-08 13:40:39 +0200

    2/2 migrations
    done with migrations

--- Blog/Migrations/Configuration.cs
+++ Blog/Migrations/Configuration.cs
@@ -5,11 +5,12 @@ namespace Blog.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Blog.Models.ApplicationDbContext>
+    public sealed class Configuration : DbMigrationsConfiguration<Blog.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            ContextKey = "Blog.Models.ApplicationDbContext";
         }
 
         protected override void Seed(Blog.Models.ApplicationDbContext context)
