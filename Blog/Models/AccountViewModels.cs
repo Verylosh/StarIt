ba383c7b6b8a6aad762c2a31272d9bc27b5ce428
Martin Shishkov <verylosh@gmail.com>
2016-12-08 13:02:31 +0200

    UserName to Register
    add UserName to Register page

--- Blog/Models/AccountViewModels.cs
+++ Blog/Models/AccountViewModels.cs
@@ -68,6 +68,11 @@ namespace Blog.Models
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 1)]
